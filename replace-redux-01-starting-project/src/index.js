7c459c5d94eca16bc912cb7d2113ff923dcbb3fc
Churycheva Nataliia <churycheva.nataliia@gmail.com>
2023-05-19 11:46:57 +0200

    CounterStore

--- replace-redux-01-starting-project/src/index.js
+++ replace-redux-01-starting-project/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 import configureProductStore from './hooks-store/product-store';
+import configureCounterStore from './hooks-store/counter-store';
 
 configureProductStore();
+configureCounterStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
